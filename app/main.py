ce286414ba13c97d0a8ef1017927578a95c0cb84
Piett-rm <rmpetit24@gmail.com>
2024-05-14 16:27:39 +0200

    feat(main): better better

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
-#better better
+#better better better
 app = FastAPI()
 
 @app.get("/http-cat/{status_code}")
